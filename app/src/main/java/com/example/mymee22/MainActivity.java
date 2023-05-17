33e20277407420f4aaa792a73baaad4aa6297376
vidhisharma3020 <vidhisharma5920@gmail.com>
2023-05-18 02:09:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mymee22/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mymee22;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+
+}
